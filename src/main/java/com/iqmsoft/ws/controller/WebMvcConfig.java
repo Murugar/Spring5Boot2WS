63a001195fa299c577f8936499a500b20f2d319b
Muruga <davanon2014@gmail.com>
2021-09-22 12:35:30 +0530

    First

--- src/main/java/com/iqmsoft/ws/controller/WebMvcConfig.java
+++ src/main/java/com/iqmsoft/ws/controller/WebMvcConfig.java
@@ -1,11 +1,18 @@
 package com.iqmsoft.ws.controller;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Controller
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+//@SuppressWarnings("deprecation")
+@Configuration
+public class WebMvcConfig extends 
+//WebMvcConfigurationSupport
+WebMvcConfigurerAdapter 
+
+{
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
